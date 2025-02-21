ba1ea50e47388002c2a740e667086a808d3535ed
david-kucera <kucera.david2002@gmail.com>
2025-02-21 09:10:59 +0100

    Init, prve cviko

---
+++ DS/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace DS;
+
+class Program
+{
+    static void Main()
+    {
+        const int pocetRep = 1_000_000;
+        Random rnd = new Random();
+        double kumVysledok = 0.0;
+
+        const double L = 5.0;
+        const double D = 10.0;
+        int pocetPretnuti = 0;
+        
+        for (int i = 1; i <= pocetRep; i++)
+        {
+            double y = rnd.NextDouble() * D;
+            double alpha = rnd.NextDouble() * Math.PI;
+            double a = L * Math.Sin(alpha);
+            
+            if (y + a >= D) pocetPretnuti++;
+        }
+        kumVysledok += pocetPretnuti / (double)pocetRep;
+        Console.WriteLine($"PI: {(2.0 * L)/(D * kumVysledok)}");
+    }
+}
