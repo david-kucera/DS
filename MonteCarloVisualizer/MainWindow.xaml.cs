7d8ce6499404ae873eaf0ce13312e6b915ea2333
david-kucera <kucera.david2002@gmail.com>
2025-03-06 15:27:47 +0100

    Fungujúce vykreslľovanie grafov ustaľovania na GUI

--- MonteCarloVisualizer/MainWindow.xaml.cs
+++ MonteCarloVisualizer/MainWindow.xaml.cs
@@ -1,75 +1,94 @@
 ﻿using System.Windows;
-using DSLib.MonteCarlo;
+using MonteCarloLib;
+using ScottPlot.WPF;
 
-namespace MonteCarloVisualizer;
-
-public partial class MainWindow : Window
+namespace MonteCarloVisualizer
 {
-	private List<double> values = new();
-	private BuffonNeedle _simulation;
-
-	public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        private MonteCarloLogic _simulation;
+        private List<double> _valuesA = new();
+        private List<double> _valuesB = new();
+        private List<double> _valuesC = new();
+        private List<double> _valuesD = new();
 
-        //StabilizationPlot1.Plot.XLabel("Iterácia");
-        //StabilizationPlot1.Plot.YLabel("Hodnota");
-        //StabilizationPlot1.Plot.Title("Graf ustáľovania");
-	}
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
 
-	private void NewData(object? sender, double e)
-	{
-		var newValue = e;
-		Application.Current.Dispatcher.Invoke(() =>
-		{
-			values.Add(newValue);
-			CurrentValueALabel.Content = $"{newValue}";
-			UpdatePlot();
-		});
-	}
+        private void StartSimulation(int numReps, int interval, int seed)
+        {
+            _valuesA.Clear();
+            _valuesB.Clear();
+            _valuesC.Clear();
+            _valuesD.Clear();
 
-	private void StartSimulation(int numReps)
-	{
-		values.Clear();
-		StabilizationPlot1.Plot.Clear();
-		StabilizationPlot1.Refresh();
+            StabilizationPlot1.Plot.Clear();
+            StabilizationPlot2.Plot.Clear();
+            StabilizationPlot3.Plot.Clear();
+            StabilizationPlot4.Plot.Clear();
 
-		Task.Run(() =>
-		{
-			double L = 5.0;
-			double D = 10.0;
-			_simulation = new(new Random(), new Random(), D, L);
-			_simulation.NewPiEstimate += NewData;
-			_simulation.Run(numReps);
-		});
-	}
+            StabilizationPlot1.Refresh();
+            StabilizationPlot2.Refresh();
+            StabilizationPlot3.Refresh();
+            StabilizationPlot4.Refresh();
 
-	private void UpdatePlot()
-	{
-		Application.Current.Dispatcher.Invoke(() =>
-		{
-			if (values.Count > 1)
-			{
-				double[] xData = Enumerable.Range(1, values.Count).Select(i => (double)i).ToArray();
-				double[] yData = values.ToArray();
+            _simulation = new MonteCarloLogic(seed);
 
-				StabilizationPlot1.Plot.Clear();
-				StabilizationPlot1.Plot.Add.Scatter(xData, yData);
-				StabilizationPlot1.Refresh();
-			}
-		});
-	}
+            _simulation.NewValueA += NewDataA;
+            _simulation.NewValueB += NewDataB;
+            _simulation.NewValueC += NewDataC;
+            _simulation.NewValueD += NewDataD;
 
-	private void RunSimulation_OnClick(object sender, RoutedEventArgs e)
-	{
-		if (int.TryParse(ReplicationCountInput.Text, out int numReps))
-		{
-			StartSimulation(numReps);
-		}
-	}
+            _simulation.Start(numReps, interval);
+        }
 
-	private void StopSimulation_OnClick(object sender, RoutedEventArgs e)
-	{
-		_simulation.NewPiEstimate -= NewData;
-	}
+        private void NewDataA(object? sender, double e) => UpdateUI(_valuesA, e, CurrentValueALabel, StabilizationPlot1);
+        private void NewDataB(object? sender, double e) => UpdateUI(_valuesB, e, CurrentValueBLabel, StabilizationPlot2);
+        private void NewDataC(object? sender, double e) => UpdateUI(_valuesC, e, CurrentValueCLabel, StabilizationPlot3);
+        private void NewDataD(object? sender, double e) => UpdateUI(_valuesD, e, CurrentValueDLabel, StabilizationPlot4);
+
+        private void UpdateUI(List<double> values, double newValue, System.Windows.Controls.Label label, WpfPlot plot)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                values.Add(newValue);
+                label.Content = $"{newValue:F4}";
+                UpdatePlot(values, plot);
+            });
+        }
+
+        private void UpdatePlot(List<double> values, WpfPlot plot)
+        {
+            if (values.Count == 0) return; 
+            
+            double[] xData = Enumerable.Range(1, values.Count).Select(i => (double)i).ToArray();
+            double[] yData = values.ToArray();
+
+            plot.Plot.Clear();
+            plot.Plot.Add.Scatter(xData, yData);
+            plot.Plot.Axes.AutoScale();
+            plot.Refresh();
+        }
+
+        private void RunSimulation_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(ReplicationCountInput.Text, out int numReps) &&
+                int.TryParse(IntervalInput.Text, out int interval) &&
+                int.TryParse(SeedInput.Text, out int seed))
+            {
+                StartSimulation(numReps, interval, seed);
+            }
+            else
+            {
+                MessageBox.Show("Zadajte platné čísla pre všetky parametre!", "Chyba", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void StopSimulation_OnClick(object sender, RoutedEventArgs e)
+        {
+            _simulation?.Stop();
+        }
+    }
 }
